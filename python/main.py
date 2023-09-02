efa0306b69c46407777134ae26e99384b4ac7c37
sudosubin <sudosubin@gmail.com>
2023-09-03 01:01:17 +0900

    1.2: convert these to bytes

--- python/main.py
+++ python/main.py
@@ -17,3 +17,16 @@ class DNSQuestion:
     name: bytes
     type_: int
     class_: int
+
+
+def header_to_bytes(header: DNSHeader):
+    fields = dataclasses.astuple(header)
+    # 6 fields
+    return struct.pack('!HHHHHH', *fields)
+
+
+def question_to_bytes(question: DNSQuestion):
+    return question.name + struct.pack("!HH", question.type_, question.class_)
+
+
+print(header_to_bytes(DNSHeader(id=0x1314, flags=0, num_questions=1, num_additionals=0, num_authorities=0, num_answers=0)))
