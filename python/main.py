ccd1e61816650778cbfc4460564256c17ce09778
sudosubin <sudosubin@gmail.com>
2023-09-03 12:51:54 +0900

    2.6: implement DNS compression

--- python/main.py
+++ python/main.py
@@ -67,23 +67,37 @@ def parse_header(reader: io.BytesIO):
 
 
 def parse_question(reader: io.BytesIO):
-    name = decode_name_simple(reader)
+    name = decode_name(reader)
     type_, class_ = struct.unpack("!HH", reader.read(4))
     return DNSQuestion(name, type_, class_)
 
 
 def parse_record(reader: io.BytesIO):
-    name = decode_name_simple(reader)
+    name = decode_name(reader)
     type_, class_, ttl, data_len = struct.unpack("!HHIH", reader.read(10))
     data = reader.read(data_len)
     return DNSRecord(name, type_, class_, ttl, data)
 
 
-def decode_name_simple(reader: io.IOBase):
+def decode_name(reader: io.IOBase):
     parts = []
     while (length := reader.read(1)[0]) != 0:
-        parts.append(reader.read(length))
+        if length & 0b1100_0000:
+            parts.append(decode_compressed_name(length, reader))
+            break
+        else:
+            parts.append(reader.read(length))
     return b".".join(parts)
+
+
+def decode_compressed_name(length: int, reader: io.IOBase):
+    pointer_bytes = bytes([length & 0b0011_1111]) + reader.read(1)
+    pointer = struct.unpack("!H", pointer_bytes)[0]
+    current_pos = reader.tell()
+    reader.seek(pointer)
+    result = decode_name(reader)
+    reader.seek(current_pos)
+    return result
 
 
 def main():
